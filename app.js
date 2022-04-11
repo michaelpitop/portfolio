e69cede38c6a855bf163b4767113d5fd61502008
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2022-04-11 16:16:14 +0200

    minor edits

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controls');
 const sectBtn = document.querySelectorAll('.control');
-const allSections = document.querySelectorAll('.main-content');
+const allSections = document.querySelector('.main-content');
 
 
 function PageTransitions() {
   // button click active class
-  for(let i = 0; i < sectBtn.length; i++){
+  for (let i = 0; i < sectBtn.length; i++){
     sectBtn[i].addEventListener('click', function(){
       let currentBtn = document.querySelectorAll('.active-btn');
       currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
       this.className += ' active-btn';
     })
   }
+
+  // sections active class
+  allSections.addEventListener('click', (e) =>{
+    console.log(e);
+    // const id = e.target.dataset.id;
+  })
 }
 
 PageTransitions();
