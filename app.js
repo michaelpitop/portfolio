55855e29ee88676ea8323cbcb1632a4db281b056
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2022-04-11 14:52:09 +0200

    added ids in each section

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelectorAll('.main-content');
 
 
-function PageTransition() {
+function PageTransitions() {
   // button click active class
-  
+  for(let i = 0; i < sectBtn.length; i++){
+    sectBtn[i].addEventListener('click', function(){
+      let currentBtn = document.querySelectorAll('.active-btn');
+      currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
+      this.className += ' active-btn';
+    })
+  }
 }
+
+PageTransitions();
