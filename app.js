c3750a18e60479fe6bebcc6614e3b52a0c54a646
Michael	Pitopoulakis michaelpitop2@gmail.com <michaelpitop2@gmail.com>
2022-04-11 14:24:36 +0200

    added a js file and transition between active button

---
+++ app.js
@@ -0,0 +1,10 @@
+const sections = document.querySelectorAll('.section');
+const sectBtns = document.querySelectorAll('.controls');
+const sectBtn = document.querySelectorAll('.control');
+const allSections = document.querySelectorAll('.main-content');
+
+
+function PageTransition() {
+  // button click active class
+  
+}
